df5ca28ecef03a74b3b9be30a170a1bcde4fdfe2
James <pigeonjim@gmail.com>
2023-04-06 18:06:31 +0100

    working the css for the calculator divs and buttons

--- calculator.js
+++ calculator.js
@@ -12,4 +12,36 @@ function onLoad(){
     mainDiv.appendChild(headerDiv);
     mainDiv.appendChild(calcDiv);
     document.body.appendChild(mainDiv);
+    addTheButtonsNScreen();
 }
+
+function addTheButtonsNScreen(){
+    const calcDiv = document.querySelector(".calcBase")
+    const screenDiv = document.createElement("div");
+    const buttonDiv= document.createElement("div");
+    screenDiv.classList = "screenDiv";
+    buttonDiv.classList = "buttonDiv";
+    calcDiv.appendChild(screenDiv); 
+    calcDiv.appendChild(buttonDiv);
+
+    for(let i = 1; i < 3; i ++){
+        const longerButton1 = document.createElement("button");
+        const longerButton2 = document.createElement("button");
+        longerButton1.classList = "buttons longerButton"
+        longerButton1.style.setProperty("grid-column","1 / 3");
+        longerButton2.classList = "buttons longerButton"
+        longerButton2.style.setProperty("grid-column","4 / 6");
+        buttonDiv.appendChild(longerButton1);
+        buttonDiv.appendChild(longerButton2);
+    }  
+    for(let i = 1; i < 21;i++){
+     
+        let theButton = document.createElement("button");
+        theButton.classList = "buttons";
+        if(i % 5 == 0 || (i + 1) % 5 == 0) {
+            theButton.classList = "buttons longerButton";
+        }
+        theButton.textContent = i;
+        buttonDiv.appendChild(theButton); }
+
+    }
